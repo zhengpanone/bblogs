07b14ee59a8687b77b71615edb4f8b26d1742317
zhengpanone <zhengpanone@hotmail.com>
2018-11-21 22:40:57 +0800

    bioinfo

--- source/conf.py
+++ source/conf.py
@@ -188,7 +188,7 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 
-#intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/': None}
 
 # -- Options for todo extension ----------------------------------------------
 
