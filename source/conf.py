614a8465512e6230f1dc406773a6d91571b97a81
zhengpanone <zhengpanone@hotmial.com>
2025-09-17 14:06:35 +0800

    提交代码

--- source/conf.py
+++ source/conf.py
@@ -12,6 +12,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import sphinx_rtd_theme
+import platform
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -81,6 +83,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
